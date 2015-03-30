041edc5c68c913607da1c570150b354386d6f14d
weichusnheng <weichunsheng3611@gmail.com>
2015-03-30 15:51:46 +0800

    temp commit ...

--- APP/app/src/androidTest/java/com/hpuvoice/app/ApplicationTest.java
+++ APP/app/src/androidTest/java/com/hpuvoice/app/ApplicationTest.java
@@ -2,6 +2,7 @@ package com.hpuvoice.app;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.Log;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +10,14 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public  void testDb(){
+        Log.e("dBTest.....","this is dBTest.....");
     }
 }
